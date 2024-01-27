09c1ab5f773a8037c4a8cdaa2b4fe9403b2ff6b2
H2Owater425 <h2o@h2owr.xyz>
2024-01-28 08:33:22 +0900

    fix: Fix robots.txt

--- source/routes/getRobotsTxt.controller.ts
+++ source/routes/getRobotsTxt.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from '@library/type';
 
 export default function (request: Request, response: Response): void {
-	response.send('User-agent: *\nAllow: /$\nDisallow: /');
+	response.send('User-agent: *\nAllow: /$\nAllow: /favicon.ico\nAllow: /ogImage.png\nDisallow: /');
 
 	return;
 }
